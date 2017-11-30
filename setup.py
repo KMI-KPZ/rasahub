773aa79a910d65fbf1c1b292820200e604e331b9
Christian Frommert <Christian.Frommert@gmail.com>
2017-11-30 12:43:43 +0100

    Prepare 0.1.0

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup, find_packages
 
+install_requires = [
+    'rasa-core',
+    'rasa_nlu',
+    'mysql-connector==2.1.4'
+]
+
+tests_requires = [
+    'pytest',
+    'testing.mysqld'
+]
+
+extras_requires = {
+    'test': tests_requires
+}
+
 setup(
     name='rasahub',
-    version='0.0.12',
+    version='0.1.0',
     description='Rasahub connects Rasa_core to Humhub Mail',
     author='Christian Frommert',
     author_email='christian.frommert@gmail.com',
@@ -15,8 +30,10 @@ setup(
         'Topic :: Software Development',
     ],
     keywords='humhub rasa rasa_core',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['rasa-core', 'rasa_nlu', 'mysql-connector==2.1.4'],
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=install_requires,
+    tests_require=tests_requires,
+    extras_require=extras_requires,
     entry_points={
        'console_scripts': [
            'rasahub = rasahub:main',
